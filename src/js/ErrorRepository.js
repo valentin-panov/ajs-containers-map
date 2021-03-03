578b1c2f7bd8d17fcee4cf82d5a5e69808cef2ea
Valentin Panov <valentin.panov@me.com>
2021-03-03 23:07:28 +0300

    added blank line

--- src/js/ErrorRepository.js
+++ src/js/ErrorRepository.js
@@ -2,6 +2,7 @@ export default class ErrorRepository {
   constructor() {
     this.errorMap = new Map();
   }
+
   translate(code) {
     const errString = this.errorMap.get(code);
     return errString ? errString : `Unknown error`;
