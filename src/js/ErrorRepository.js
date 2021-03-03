693a83cf6bb973c4fa0b48ac64d2ff51de5f719e
Valentin Panov <valentin.panov@me.com>
2021-03-03 23:00:52 +0300

    init comm

---
+++ src/js/ErrorRepository.js
@@ -0,0 +1,9 @@
+export default class ErrorRepository {
+  constructor() {
+    this.errorMap = new Map();
+  }
+  translate(code) {
+    const errString = this.errorMap.get(code);
+    return errString ? errString : `Unknown error`;
+  }
+}
